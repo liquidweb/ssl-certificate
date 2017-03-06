8c1f1f7991c60b6475c50a8d1720140c58fb682c
Laurence Ioannou <github@theshiftexchange.com>
2017-03-06 23:08:53 +0000

    Update SslCertificate.php

--- src/SslCertificate.php
+++ src/SslCertificate.php
@@ -280,7 +280,7 @@ class SslCertificate
 
     public function isValidUntil(Carbon $carbon, string $url = null): bool
     {
-        if ($this->expirationDate()->gt($carbon)) {
+        if ($this->expirationDate()->lt($carbon)) {
             return false;
         }
 
