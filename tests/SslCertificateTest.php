550373a66f2455de58e2442895ab443ffcbd830e
peter279k <peter279k@gmail.com>
2020-02-29 16:19:22 +0800

    Test improvement

--- tests/SslCertificateTest.php
+++ tests/SslCertificateTest.php
@@ -11,7 +11,7 @@ class SslCertificateTest extends TestCase
     /** @var SslCertificate */
     protected $certificate;
 
-    public function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
